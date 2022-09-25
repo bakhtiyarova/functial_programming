ce73e2592d74c3ae54f3917a01a023d5e2b1cfcc
bakhtiyarova <75243374+bakhtiyarova@users.noreply.github.com>
2022-09-26 00:34:24 +0600

    Random

--- lab3.py
+++ lab3.py
@@ -1,7 +1,14 @@
 import random
-a=list()
+def Random(n):
+    a=list()
+    for i in range(n):
+        b=random.randint(1,10)
+        a.append(b)
+    return a
+def min(a):
+    a.sort()
+    return a
 n=int(input())
-for i in range(n):
-    b=random.randint(10,100)
-    a.append(b)
-print(a)
+print(Random(n))
+print(min(Random(n)))
+

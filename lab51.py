1996cca44ba16ee78304362f2882dce692346671
bakhtiyarova <75243374+bakhtiyarova@users.noreply.github.com>
2022-10-13 01:33:26 +0600

    New

--- lab51.py
+++ lab51.py
@@ -1,21 +1,24 @@
-from audioop import reverse
-
-def check(l,newl):
-    bo=True
-    for i in range(len(l)):
-        if l[i]==newl[i]:
-            bo=False
-    return bo
-l=list()
-for i in range(5):
-    print("Enter number:")
-    n=int(input())
-    l.append(n)
-a=l
-print(a)
-l.sort()
-b=check(a,l)
-if b==True: print("List is sorted")
-else: print("List is not sorted")
-
+class Student:
+    name=""
+    course_name=""
+    course=0
+    def printInfo(self):
+        return "Name: "+self.name+", Course: "+str(self.course)+", Course name: "
+students=list()
+for i in range(2):
+    student=Student()
+    print("________________")
+    print("Enter name: ")
+    student.name=input()
+    print("Enter course: ")
+    student.course=int(input())
+    students.append(student)
+for i in range(len(students)):
+    for j in range(len(students)):
+        if students[i].course<students[j].course:
+            temp=students[i]
+            students[i]=students[j]
+            students[j]=temp
+for student in students:
+    print(student.printInfo())
 

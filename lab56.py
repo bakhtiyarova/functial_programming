c4cc1848fee2dbfc71f9c786114b7a942bc4de6c
bakhtiyarova <75243374+bakhtiyarova@users.noreply.github.com>
2022-10-13 11:34:32 +0600

    Add files via upload

---
+++ lab56.py
@@ -0,0 +1,21 @@
+def check(l):
+  a=list()
+  for j in range(len(l)):
+    a.append(l[j])
+  print(a)
+  l.sort()
+  for i in l:
+    if l.index(i)==a.index(i):
+      bo=True
+    else:
+      bo=False
+  return bo
+l=list()
+while True:
+  n=int(input())
+  if n==0: break
+  l.append(n)
+if check(l):
+  print("List is sorted: ")
+else:
+  print("List is not sorted: ")
